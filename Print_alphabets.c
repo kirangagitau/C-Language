d71189f521c5467ed69ab420f031a7f2aa72ea24
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-02-20 11:55:55 -0800

    print alphabets

---
+++ Print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *@Author Gitau Muguro
+ *
+ * main - A program to alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+        char c;
+
+        c = 'a';
+        while
+                (c <= 'z')
+                {
+                        putchar(c);
+                        c++;
+                }
+        putchar('\n');
+        return (0);
