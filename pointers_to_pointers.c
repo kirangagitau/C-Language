65ff5c75b5fc52c46a2b423031990cd69a135145
Gitau Muguro <121950164+kirangagitau@users.noreply.github.com>
2023-05-26 06:27:17 -0700

    pointers_to_pointers.c

--- pointers_to_pointers.c
+++ pointers_to_pointers.c
@@ -1,1 +1,32 @@
+/**
+* pointer - method to deal with pointers
+* main - of type int, this is main function.
+* @ans: the working around to, answer.
+* @ptr: first pointer.
+* @ptr1: second pointer (pointer to pointer).
+*/
 
+
+int pointer();
+
+int main()
+{
+    int ans;
+
+    ans = pointer();
+    printf("the answer is: %d \n",ans);
+
+    return 0;
+}
+
+int pointer()
+{
+    int ans = 123;
+    int* ptr;
+    int** ptr1;
+
+    ptr = &ans;
+    ptr1 = ptr;
+  
+  return (*ptr1);
+}
